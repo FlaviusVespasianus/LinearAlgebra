da7f791c69c36e6be070ba21a8862b8405e90095
FlaviusVespasianus <antoninusheliogabalus@gmail.com>
2016-12-13 11:34:56 +0300

    v2 --final

--- transpose-matrix.php
+++ transpose-matrix.php
@@ -1,31 +1,22 @@
+<?php
 //take a matrix and returns a transposed one
+//matrix ought to be an array in array, 
+//so the smallest matrix, which consists of one element,
+//looks like: [ [ 0 ] ];
 
-$a = [ [1, 2],
-       [3, 4] ];
-       
 function transpose($matrix){
-    //only for square ones for now
     if (count($matrix) == 1){
         for ($i = 0; $i < count($matrix[0]); $i++){
             $transposed[$i][] = $matrix[0][$i];
         }
         return $transposed;
-    } elseif (count($matrix) == 2) {
-        $row1 = transpose([$matrix[0]]);
-        foreach ($matrix[1] as $position => $element){
-            $row1[$position][] = $element;
-        }
-        $transposed = $row1;
-        return $transposed;
     } else {
-        $row1 = transpose([$matrix[0]]);
+        $transposed = transpose([$matrix[0]]);
         for ($m = 1; $m < count($matrix); $m++){
             foreach ($matrix[$m] as $position => $element) {
-                $row1[$position][] = $element;
+                $transposed[$position][] = $element;
             }
         }
-        $transposed = $row1;
         return $transposed;
     }
-
 }
