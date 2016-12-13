0ba4fa6e2cf6051370e00581305efd377cbae6d3
FlaviusVespasianus <antoninusheliogabalus@gmail.com>
2016-12-13 10:19:30 +0300

    13.12 need to make function transpose first

---
+++ adjugate-matrix.php
@@ -0,0 +1,22 @@
+// this pogramme computes an adjugate matrix for any matrix given
+// matrices are taken from the code itself
+
+$a = [ [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9] ];
+
+$b = [ [1, 2],
+       [3, 4]];
+
+function adjugate($matrix){
+//check here if matrix is square
+    if (count($matrix) == 1){
+        $adjoint = $matrix;
+        return $adjoint;
+    } elseif (count($matrix) == 2){
+
+    } elseif (count($matrix) > 2) {
+
+
+        return
+      
