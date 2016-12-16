2de01d8ea5d28630e4ee47e3fc51544ddc9816e2
FlaviusVespasianus <antoninusheliogabalus@gmail.com>
2016-12-16 10:20:34 +0300

    v1

--- printMatrix.php
+++ printMatrix.php
@@ -1,3 +1,22 @@
 <?php
 
 //prints matrix on the screen
+
+function showMatrix($matrix){
+    $max = 0;
+    foreach ($matrix as $row){
+        foreach ($row as $element){
+            if (strlen($element) > $max){
+                $max = strlen($element);
+            }
+        }
+    }
+    foreach ($matrix as $row){
+        print "<pre>";
+        foreach ($row as $element){
+            $string = str_pad($element, $max + 1, ' ', STR_PAD_BOTH);
+             print "$string";
+        }
+        print "</pre>";
+    }
+}
