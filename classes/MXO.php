cc328b2acc0b3f7cecbe79b95ad4dbb15e6be6cb
FlaviusVespasianus <antoninusheliogabalus@gmail.com>
2016-12-17 18:09:03 +0300

    added classes

---
+++ classes/MXO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauri
+ * Date: 17/12/2016
+ * Time: 18:07
+ */
+
+namespace LinearAlgebra;
+
+//Matrix Operations class
+class MXO
+{
+
+}
---
+++ classes/Matrix.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LinearAlgebra;
+
+//Matrix
+class MX
+{
+
+}
